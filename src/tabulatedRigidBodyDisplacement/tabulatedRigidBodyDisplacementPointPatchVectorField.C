8a041409600f24a5d39e4534ed8f7c8c1c552b4e
Tomislav Maric <tomislav.maric@gmx.com>
2013-06-12 21:47:14 +0200

    Added Kyle's tabulatedRigidBodyDisplacementPointPatchVectorField BC.

---
+++ src/tabulatedRigidBodyDisplacement/tabulatedRigidBodyDisplacementPointPatchVectorField.C
@@ -0,0 +1,191 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "tabulatedRigidBodyDisplacementPointPatchVectorField.H"
+#include "pointPatchFields.H"
+#include "addToRunTimeSelectionTable.H"
+#include "Time.H"
+#include "fvMesh.H"
+#include "volFields.H"
+#include "uniformDimensionedFields.H"
+#include "forces.H"
+#include "tabulated6DoFMotion.H"
+#include "Tuple2.H"
+#include "IFstream.H"
+#include "interpolateSplineXY.H"
+#include "mathematicalConstants.H"
+#include "transformField.H"
+
+using namespace Foam::constant::mathematical;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+tabulatedRigidBodyDisplacementPointPatchVectorField::
+tabulatedRigidBodyDisplacementPointPatchVectorField
+(
+    const pointPatch& p,
+    const DimensionedField<vector, pointMesh>& iF
+)
+:
+    fixedValuePointPatchField<vector>(p, iF)
+{}
+
+
+tabulatedRigidBodyDisplacementPointPatchVectorField::
+tabulatedRigidBodyDisplacementPointPatchVectorField
+(
+    const pointPatch& p,
+    const DimensionedField<vector, pointMesh>& iF,
+    const dictionary& dict
+)
+:
+    fixedValuePointPatchField<vector>(p, iF, dict)
+{
+        updateCoeffs();
+}
+
+
+tabulatedRigidBodyDisplacementPointPatchVectorField::
+tabulatedRigidBodyDisplacementPointPatchVectorField
+(
+    const tabulatedRigidBodyDisplacementPointPatchVectorField& ptf,
+    const pointPatch& p,
+    const DimensionedField<vector, pointMesh>& iF,
+    const pointPatchFieldMapper& mapper
+)
+:
+    fixedValuePointPatchField<vector>(ptf, p, iF, mapper)
+{}
+
+
+tabulatedRigidBodyDisplacementPointPatchVectorField::
+tabulatedRigidBodyDisplacementPointPatchVectorField
+(
+    const tabulatedRigidBodyDisplacementPointPatchVectorField& ptf,
+    const DimensionedField<vector, pointMesh>& iF
+)
+:
+    fixedValuePointPatchField<vector>(ptf, iF)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void tabulatedRigidBodyDisplacementPointPatchVectorField::autoMap
+(
+    const pointPatchFieldMapper& m
+)
+{
+    fixedValuePointPatchField<vector>::autoMap(m);
+
+}
+
+
+void tabulatedRigidBodyDisplacementPointPatchVectorField::rmap
+(
+    const pointPatchField<vector>& ptf,
+    const labelList& addr
+)
+{
+    const tabulatedRigidBodyDisplacementPointPatchVectorField& sDoFptf =
+        refCast<const tabulatedRigidBodyDisplacementPointPatchVectorField>(ptf);
+
+    fixedValuePointPatchField<vector>::rmap(sDoFptf, addr);
+
+}
+
+
+void tabulatedRigidBodyDisplacementPointPatchVectorField::updateCoeffs()
+{
+    if (this->updated())
+    {
+        return;
+    }
+
+    const polyMesh& mesh = this->dimensionedInternalField().mesh()();
+    const Time& t = mesh.time();
+    const pointPatch& ptPatch = this->patch();
+
+    const dictionary  dynamicMeshCoeffs
+    (
+        IOdictionary
+        (
+            IOobject
+            (
+                "dynamicMeshDict",
+                t.constant(),
+                mesh,
+                IOobject::MUST_READ_IF_MODIFIED,
+                IOobject::NO_WRITE,
+                false
+            )
+        ).subDict("tabulated6DoFMotionCoeffs")
+    );
+
+    autoPtr<solidBodyMotionFunction> SBMFPtr(solidBodyMotionFunction::New(dynamicMeshCoeffs, t));
+
+    pointField vectorIO(mesh.points().size(),vector::zero);
+
+    vectorIO = transform
+        (
+            SBMFPtr().transformation(),
+            ptPatch.localPoints()
+        );
+
+    Field<vector>::operator=
+    (
+              vectorIO-ptPatch.localPoints()
+    );
+
+    fixedValuePointPatchField<vector>::updateCoeffs();
+}
+
+
+void tabulatedRigidBodyDisplacementPointPatchVectorField::write(Ostream& os) const
+{
+    pointPatchField<vector>::write(os);
+
+    writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+makePointPatchTypeField
+(
+    pointPatchVectorField,
+    tabulatedRigidBodyDisplacementPointPatchVectorField
+);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
