a5b8447c0d65084ca58a8ec5fbf002fd9693c8b7
Kyle Mooney <kyle.g.mooney@gmail.com>
2013-07-29 14:17:28 -0400

    Initial commit of the new viscosity model stuff

---
+++ src/ofBookTransportModels/interpolatedSplineViscosityModel/interpolatedSplineViscosityModel.C
@@ -0,0 +1,177 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "interpolatedSplineViscosityModel.H"
+#include "addToRunTimeSelectionTable.H"
+#include "surfaceFields.H"
+#include "interpolateSplineXY.H"
+#include "mathematicalConstants.H"
+#include "Tuple2.H"
+#include "IFstream.H"
+#include "volFields.H"
+#include "fvcGrad.H"
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace viscosityModels
+{
+    defineTypeNameAndDebug(interpolatedSplineViscosityModel, 0);
+
+    addToRunTimeSelectionTable
+    (
+        viscosityModel,
+        interpolatedSplineViscosityModel,
+        dictionary
+    );
+}
+}
+
+
+// * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * * //
+
+Foam::volScalarField
+Foam::viscosityModels::interpolatedSplineViscosityModel::calcNu()
+{
+
+    loadDataTable();
+
+    volScalarField viscosityField
+    (
+        IOobject
+        (
+            "viscosityField",
+            U_.time().timeName(),
+            U_.db(),
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        U_.mesh(),
+        dimensionedScalar("viscosityField", dimensionSet(0,2,-1,0,0,0,0), 0.0),
+        zeroGradientFvPatchScalarField::typeName
+    );
+
+    const volScalarField& localStrainRate = strainRate();
+
+    forAll(localStrainRate.internalField(),cellI)
+    {
+        scalar& localMu = viscosityField.internalField()[cellI];
+              
+        localMu = interpolateSplineXY
+        (
+            localStrainRate.internalField()[cellI],
+            rheologyTableX_,
+            rheologyTableY_
+        );  
+    }
+
+    return viscosityField;
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::viscosityModels::interpolatedSplineViscosityModel::interpolatedSplineViscosityModel
+(
+    const word& name,
+    const dictionary& viscosityProperties,
+    const volVectorField& U,
+    const surfaceScalarField& phi
+)
+:
+    viscosityModel(name, viscosityProperties, U, phi),
+    interpolatedSplineViscosityModelCoeffs_(viscosityProperties.subDict(typeName + "Coeffs")),
+    rheologyTableX_(label(1),scalar(0)),
+    rheologyTableY_(label(1),scalar(0)),
+    dataFileName_(interpolatedSplineViscosityModelCoeffs_.lookup("dataFileName")),
+    U_(U),
+    nu_
+    (
+        IOobject
+        (
+            name,
+            U_.time().timeName(),
+            U_.db(),
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        calcNu()
+    )
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+bool Foam::viscosityModels::interpolatedSplineViscosityModel::read
+(
+    const dictionary& viscosityProperties
+)
+{
+    viscosityModel::read(viscosityProperties);
+
+    interpolatedSplineViscosityModelCoeffs_ = viscosityProperties.subDict(typeName + "Coeffs");
+    interpolatedSplineViscosityModelCoeffs_.lookup("dataFileName") >> dataFileName_;
+
+    return true;
+}
+
+void Foam::viscosityModels::interpolatedSplineViscosityModel::loadDataTable()
+{
+
+    fileName timeDataFileName
+    (
+       dataFileName_
+    );
+
+    IFstream dataStream(timeDataFileName);
+
+    if (dataStream.good())
+    {
+        List<Tuple2<scalar, scalar> > timeValues
+        (
+            dataStream
+        );
+
+        rheologyTableX_.setSize(timeValues.size());
+        rheologyTableY_.setSize(timeValues.size());
+
+        forAll(timeValues, i)
+        {
+            rheologyTableX_[i] = timeValues[i].first();
+            rheologyTableY_[i] = timeValues[i].second();
+        }
+    }
+    else
+    {
+        FatalErrorIn
+        (
+            "viscosityModels::splineInterpolated::loadDataTable"
+            "(const dictionary&)"
+        )   << "Cannot open time data file " << timeDataFileName
+           << exit(FatalError);
+    }
+
+}
+// ************************************************************************* //
