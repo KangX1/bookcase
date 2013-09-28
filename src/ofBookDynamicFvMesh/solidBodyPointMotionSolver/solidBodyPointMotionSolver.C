076f6ed97d1bc4d487143f43c4a9f0e413102996
Tomislav Maric <tomislav.maric@gmx.com>
2013-09-28 16:19:31 +0200

    Compiling solidBodyPointMotionSolver.

---
+++ src/ofBookDynamicFvMesh/solidBodyPointMotionSolver/solidBodyPointMotionSolver.C
@@ -0,0 +1,95 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Authors
+    Tomislav Maric tomislav<<dot>>maric<<a>>gmx<<dot>>com
+
+\*---------------------------------------------------------------------------*/
+
+#include "solidBodyPointMotionSolver.H"
+#include "volFields.H"
+#include "transformField.H"
+#include "boolList.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(solidBodyPointMotionSolver, 0);
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::solidBodyPointMotionSolver::solidBodyPointMotionSolver(const IOobject& io)
+:
+    dynamicMeshCoeffs_
+    (
+        IOdictionary
+        (
+            IOobject
+            (
+                "dynamicMeshDict",
+                io.time().constant(),
+                io.time(),
+                IOobject::MUST_READ_IF_MODIFIED,
+                IOobject::NO_WRITE,
+                false
+            )
+        ).subDict("solidBodyMotionFvMeshCoeffs") 
+    ),
+    SBMFPtr_(solidBodyMotionFunction::New(dynamicMeshCoeffs_, io.time()))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::solidBodyPointMotionSolver::~solidBodyPointMotionSolver()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::pointField> 
+Foam::solidBodyPointMotionSolver::movePoints(const pointField& points) const
+{
+    return transform(SBMFPtr_().transformation(), points);
+
+    // FIXME: used with the mesh 
+    //if (foundObject<volVectorField>("U"))
+    //{
+        //const_cast<volVectorField&>(lookupObject<volVectorField>("U"))
+            //.correctBoundaryConditions();
+    //}
+    //else if (!hasWarned)
+    //{
+        //hasWarned = true;
+
+        //WarningIn("solidBodyPointMotionSolver::update()")
+            //<< "Did not find volVectorField U."
+            //<< " Not updating U boundary conditions." << endl;
+    //}
+}
+
+
+// ************************************************************************* //
